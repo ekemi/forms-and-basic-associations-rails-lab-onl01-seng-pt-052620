c7d97970a6712cc79623b3c38ef3cbe3d89ff8f0
edmundo papi <edmundopierre@yahoo.com>
2020-11-06 07:19:18 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -14,4 +14,18 @@ class Song < ActiveRecord::Base
  def genre_name
    self.genre.name if self.genre
  end
+ def genre_name=(name)
+   self.genre = Genre.find_or_create_by(name:name)
+ end
+ def note_contents
+   self.notes.collect(&:content)
+ end
+ def note_contents=(note_contents)
+   note_contents.each do |content|
+     unless content.empty?
+       self.notes<<Note.create(content: content)
+       self.save
+ end
+end
+end
 end
